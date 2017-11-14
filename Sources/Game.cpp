4e092ecd7d1520f67a7e16d5ac8c0e4d7eaf7da5
hw16a093 <hw16a093@oecu.jp>
2017-11-14 15:41:12 +0900

    弾のスピードを速くした

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -4,7 +4,7 @@
 // TODO: 砲台の位置を画面左に、ターゲットの位置を画面右に移動させる。(A)
 // TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)
 // TODO: 砲台を青い壁に沿って上下に動かす。(C)
-// TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)
+// TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)(HW16A093 島村 幸佑)
 // TODO: スコアのサイズを大きくする。(E)
 // TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)
 // TODO: PlayBGM()関数を使って、BGMを再生する。(G)
@@ -38,7 +38,7 @@ void Update()
 
     // 弾の移動
     if (bulletPos.x > -999) {
-        bulletPos.x += 10 * Time::deltaTime;
+        bulletPos.x += 150 * Time::deltaTime;
 
         // ターゲットと弾の当たり判定
         Rect bulletRect(bulletPos, Vector2(32, 20));
